7e2c4be12e240d02404d27ba989c694ae01a181f
snippercomment <minhthuanad2002@gmail.com>
2025-06-05 20:03:37 +0700

    update checkout

--- app/(checkout)/checkout-success/page.jsx
+++ app/(checkout)/checkout-success/page.jsx
@@ -11,14 +11,12 @@ export default function Page({searchParams}){
            <div className="flex justify-center w-full">
                <img src="/svgs/Mobile payments-rafiki.svg" className="h-48" alt="" />
            </div>
-            <h1 className="text-2xl font-semibold">Thanh toán của bạn không thành công</h1>
+            <h1 className="text-2xl font-semibold">Đơn hàng của bạn đã được đặt thành công</h1>
             <div className="flex item-center gap-4 text-sm">
-                <Link href={"/"}>
-                    <button className="text-blue-600 border border-blue-600 px-4 py-2 rounded-lg bg-white">Cửa hàng</button>
+                <Link href={"/account"}>
+                    <button className="text-blue-600 border border-blue-600 px-4 py-2 rounded-lg bg-white">Đi đến trang đơn hàng</button>
                 </Link>
-                 <Link href={"/"}>
-                    <button className="text-blue-600 borde px-4 py-2 rounded-lg bg-white">Tải lại</button>
-                </Link>
+               
             </div>
         </section>
         <Footer/>
